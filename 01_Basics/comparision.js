6a3e96450feb6249e392d1c70510008ddb0c3e1e
Nijan <ngouda@accurate.com>
2025-01-19 16:38:20 +0000

    learnt conversion and comparision

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,20 @@
+
+// Comparing String and number
+console.log("****** String & number *************")
+console.log("2" > 1);
+console.log("02" > 1);
+
+//  null comparison
+console.log("****** null *************")
+console.log(null > 0)
+
+console.log(null == 0)
+
+console.log (null >= 0)
+
+
+// undefined
+console.log("****** undefined *************")
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined >=0)
--- 01_Basics/conversionOperation.js
+++ 01_Basics/conversionOperation.js
@@ -1,4 +1,22 @@
 
+let data=null
+let numberConversion=Number(data)
+console.log(numberConversion)
+data=undefined
+numberConversion=Number(data)
+console.log(numberConversion)
+data="22"
+numberConversion=Number(data)
+console.log(numberConversion)
+data="22a"
+numberConversion=Number(data)
+console.log(numberConversion)
+
+data=""
+numberConversion=Number(data)
+console.log(numberConversion)
+
+console.log("conversion from different types")
 // ***************** Operation ******************************
 
 let value=3
