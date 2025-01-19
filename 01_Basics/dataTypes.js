f0c4c1472a40f4eecf3eada899f9a598a845bdf6
Nijan <ngouda@accurate.com>
2025-01-19 17:15:53 +0000

    learnt data types with heap and stack

--- 01_Basics/dataTypes.js
+++ 01_Basics/dataTypes.js
@@ -44,5 +44,34 @@ console.log("MyFunction");
 
 console.log(typeof myFunction)
 
+/* datatypes are based on memory allocation
+Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+JavaScript is a dynamically typed language. This means that you don't need to specify the data type of a variable when you declare it.
+
+ Primitive - will get the copy of value
+ Non-Primitve - will get original memory(Changes will be affcted to original varaible)
+*/
+
+let user1= {
+    name: "Nijan",
+    upi: "nijan@ybl"
+}
+
+let user2=user1
+console.log("*********** After assigning user1 to user 2");
+
+console.log(user1);
+console.log(user2);
+
+user2.name="Nijan Updated"
+
+console.log("*********** After changing the value  ***********");
+
+console.log(user1);
+console.log(user2);
+
 
 
