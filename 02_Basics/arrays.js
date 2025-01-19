d5f499e7559d430502b27633d3d5b8574060c482
Nijan <ngouda@accurate.com>
2025-01-19 18:14:15 +0000

    learnt array

---
+++ 02_Basics/arrays.js
@@ -0,0 +1,17 @@
+
+/*
+  1. Are resizable
+  2. contain mix dat types
+  3. array copy operation are shallow copies
+*/
+const myArra= [0,1,2,3,4,5]
+
+const myArr2=new Array(1,2,3,4)
+
+console.log(typeof myArra)
+
+console.log(myArr2);
+
+myArr2.push(6)
+
+console.log(myArr2)
