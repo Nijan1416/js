a02fbcd2a01b19b9bfbb19d7a5b900234068c038
Nijan <ngouda@accurate.com>
2025-01-19 17:32:38 +0000

    learnt string

---
+++ 01_Basics/strings.js
@@ -0,0 +1,15 @@
+/*
+   1. ""
+   2. ''
+   3. `` - back track - string interpolation
+   4. new String()
+*/
+let name="Nijan"
+let repoCount=50
+console.log(`Hello my nam is ${name} and my repocCount is ${repoCount}` )
+
+const testName=new String("Nijannagouda")
+console.log(testName.__proto__)
+
+
+
