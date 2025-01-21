3083718b32378b6454364c502b9abd2d81a9f9f2
Nijan <ngouda@accurate.com>
2025-01-21 16:59:40 +0000

    learnt functions

---
+++ 03_Basics/functions.js
@@ -0,0 +1,12 @@
+function addNumbers(number1, number2){
+ console.log(number1+number2);
+}
+
+addNumbers(5,4)
+// ... rest operator 
+function calculate(val1,val2,...val){
+ return val;
+}
+
+console.log(calculate(1,2,3))
+
